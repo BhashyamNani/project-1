4885b2ea1007d33f9f6ab465c549b8b63b65fcb0
BHASHYAM NANI <nani.b123624@gmail.com>
2022-06-07 23:08:10 +0530

    Added nani

---
+++ nani.php
@@ -0,0 +1,20 @@
+<?php
+$newva="WELCOME NANI \n";
+$check=3;
+if($check==3)
+{
+    echo $newva;
+}
+elseif($check>=4)
+{
+    echo "second result";
+}
+else{
+    echo "exit";
+}
+while($check<7)
+{
+echo $check."\n";
+$check++;
+}
+?>
